539bf48222493865952504647eac4b5aa200a83b
andre <andresro0198@gmail.com>
2021-01-28 23:23:06 +0100

    28/01 BackStack fixed - NavHost working properly

--- app/src/main/java/cat/copernic/groupz/ui/activities/home/HomeActivity.kt
+++ app/src/main/java/cat/copernic/groupz/ui/activities/home/HomeActivity.kt
@@ -3,6 +3,8 @@ package cat.copernic.groupz.ui.activities.home
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import cat.copernic.groupz.R
 
 class HomeActivity : AppCompatActivity() {
@@ -13,6 +15,10 @@ class HomeActivity : AppCompatActivity() {
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         supportActionBar!!.hide()
         setContentView(R.layout.activity_home)
-
+        setupActionBarWithNavController(findNavController(R.id.home_fragment_container))
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.home_fragment_container)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
