6983f9a6b0f7d2d2bc92c9154db9c5f127c7a5d5
Alejandro Jimenez Sanchez <jimenez.sanchez.alejandro@alumnat.copernic.cat>
2021-02-02 20:22:28 +0100

    He creado un fragment para eliminar cuenta mas el popup funcional todo

--- app/src/main/java/cat/copernic/groupz/ui/activities/home/fragments/Confirmar_Eliminar.kt
+++ app/src/main/java/cat/copernic/groupz/ui/activities/home/fragments/Confirmar_Eliminar.kt
@@ -1,20 +1,20 @@
 package cat.copernic.groupz.ui.activities.home.fragments
+
 import android.app.AlertDialog
 import android.os.Bundle
 import android.util.Log
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import cat.copernic.groupz.R
-import cat.copernic.groupz.databinding.FragmentLogOutBinding
+import cat.copernic.groupz.databinding.FragmentConfirmarEliminarBinding
 import com.google.firebase.auth.FirebaseAuth
 
 
-
-class LogOutFragment : Fragment() {
-    private lateinit var binding: FragmentLogOutBinding
+class Confirmar_Eliminar : Fragment() {
+    private lateinit var binding: FragmentConfirmarEliminarBinding
     private lateinit var builder: AlertDialog.Builder
     var TAG : String = "Log auth Fragment"
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,29 +22,38 @@ class LogOutFragment : Fragment() {
         arguments?.let {
         }
     }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_log_out, container, false)
+        return inflater.inflate(R.layout.fragment_confirmar__eliminar, container, false)
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding = FragmentLogOutBinding.bind(view)
+        binding = FragmentConfirmarEliminarBinding.bind(view)
         builder = AlertDialog.Builder(context) //Preparamos el Alert dialog
         builder.setTitle("Log Out")
         builder.setPositiveButton("Aceptar", null)
-        binding.btnLogOut.setOnClickListener {
-            FirebaseAuth.getInstance().signOut()
-            builder.setMessage(R.string.LogOut);
-            val dialog = builder.create()
-            dialog.show()
+        binding.btnNo.setOnClickListener{
+
             findNavController().navigate(R.id.action_logOutFragment_to_login)
         }
-        binding.btnDelAccount.setOnClickListener{
-            findNavController().navigate(R.id.action_logOutFragment_to_confirmar_Eliminar)
-        }
+        binding.btnSi.setOnClickListener{
+            val user =  FirebaseAuth.getInstance().currentUser
 
+            user?.delete()
+                ?.addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        builder.setMessage(R.string.accountDeleted);
+                        val dialog = builder.create()
+                        dialog.show()
+                        Log.d(TAG, "User account deleted.")
+                    }
+                }
+            findNavController().navigate(R.id.action_logOutFragment_to_login)
+        }
     }
+
 }
