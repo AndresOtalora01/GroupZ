af773c20203f8b4b2ca11fe604e3f26e5ba900b6
andre <andresro0198@gmail.com>
2021-02-03 18:10:05 +0100

    String problematica - solved

--- app/src/main/java/cat/copernic/groupz/ui/activities/home/fragments/ForgottenPasswordFragment.kt
+++ app/src/main/java/cat/copernic/groupz/ui/activities/home/fragments/ForgottenPasswordFragment.kt
@@ -1,12 +1,13 @@
-package cat.copernic.groupz
+package cat.copernic.groupz.ui.activities.home.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import cat.copernic.groupz.R
 
-class forgotten_password : Fragment() {
+class ForgottenPasswordFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
