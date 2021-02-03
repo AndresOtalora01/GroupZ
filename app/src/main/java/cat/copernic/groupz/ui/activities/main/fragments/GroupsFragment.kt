fafdf0a80083bb36277d471777e0c59d4ed9450f
Alejandro Jimenez Sanchez <jimenez.sanchez.alejandro@alumnat.copernic.cat>
2021-02-03 20:28:10 +0100

    Añadido creacion de grupo correccion de pequeños detalles

--- app/src/main/java/cat/copernic/groupz/ui/activities/main/fragments/GroupsFragment.kt
+++ app/src/main/java/cat/copernic/groupz/ui/activities/main/fragments/GroupsFragment.kt
@@ -5,15 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import cat.copernic.groupz.R
+import cat.copernic.groupz.databinding.FragmentDeleteConfirmationBinding
+import cat.copernic.groupz.databinding.FragmentGroupsBinding
 
 
 class GroupsFragment : Fragment() {
+    private lateinit var binding: FragmentGroupsBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_groups, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentGroupsBinding.bind(view)
+    binding.btnCrearGroup.setOnClickListener{
+        findNavController().navigate(R.id.action_groupsFragment_to_create_GroupFragment)
+    }
+
     }
 }
