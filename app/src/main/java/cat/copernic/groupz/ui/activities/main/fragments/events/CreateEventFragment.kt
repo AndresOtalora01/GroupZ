f9a2ce4467759a9591ee49d45f040da0a6740674
piqueras.lopez.guillem <piqueras.lopez.guillem@alumnat.copernic.cat>
2021-02-04 15:32:50 +0100

    se han cambiado las id del boton de guardado de deitar perfil i de crear grupo ya que eran la misma i se a coregido un errol de color cuando se tenia el movil en modo oscuro

--- app/src/main/java/cat/copernic/groupz/ui/activities/main/fragments/events/CreateEventFragment.kt
+++ app/src/main/java/cat/copernic/groupz/ui/activities/main/fragments/events/CreateEventFragment.kt
@@ -1,15 +1,16 @@
 package cat.copernic.groupz.ui.activities.main.fragments.events
 
+import android.os.Binder
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import cat.copernic.groupz.R
+import cat.copernic.groupz.databinding.FragmentCreateEventBinding
 
 
 class CreateEventFragment : Fragment() {
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -17,5 +18,4 @@ class CreateEventFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_create_event, container, false)
     }
-
 }
