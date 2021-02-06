d3597114d98261789f7572baf81a0981f775de4c
andre <andresro0198@gmail.com>
2021-02-06 14:08:50 +0100

    pop up de "friendzone ha crecido" añadido - onClick listener en el botón like de swipe

--- app/src/main/java/cat/copernic/groupz/ui/activities/main/fragments/SwipeFragment.kt
+++ app/src/main/java/cat/copernic/groupz/ui/activities/main/fragments/SwipeFragment.kt
@@ -1,15 +1,19 @@
 package cat.copernic.groupz.ui.activities.main.fragments
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.fragment.app.Fragment
 import cat.copernic.groupz.R
 
 
 class SwipeFragment : Fragment() {
-
+     lateinit var dialog : Dialog
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -18,4 +22,19 @@ class SwipeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_swipe, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        var buttonDialog = view.findViewById<ImageButton>(R.id.btnLike)
+
+        dialog = context?.let { Dialog(it) }!!
+        buttonDialog.setOnClickListener {
+            openMatchDialog()
+        }
+    }
+
+    private fun openMatchDialog() {
+        dialog.setContentView(R.layout.friendzone_dialog_layout)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.show()
+    }
 }
